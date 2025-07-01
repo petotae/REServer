2a886ae5aef1b00ca870441a8a34f9954d3ccffe
Srijith Gomattam <srijith.gomattam@gmail.com>
2025-07-01 11:59:19 +1000

    Updated HomeSale with 'setters' and 'getters'

--- src/main/java/sales/HomeSale.java
+++ src/main/java/sales/HomeSale.java
@@ -3,9 +3,9 @@ package sales;
 // Simple class to provide test data in SalesDAO
 
 public class HomeSale {
-    public  String saleID;
-    public  String postcode;
-    public  String salePrice;
+    public String saleID;
+    public String postcode;
+    public String salePrice;
 
     public HomeSale(String saleID, String postcode, String salePrice) {
         this.saleID = saleID;
@@ -14,7 +14,31 @@ public class HomeSale {
     }
 
     // needed for JSON conversion
-    public HomeSale() {}
+    public HomeSale() {
+    }
 
+    public String getID() {
+        return this.saleID;
+    }
+
+    public void setID(String id) {
+        this.saleID = id;
+    }
+
+    public String getPostCode() {
+        return this.postcode;
+    }
+
+    public void setPostCode(String pc) {
+        this.postcode = pc;
+    }
+
+    public String getSalePrice() {
+        return this.salePrice;
+    }
+
+    public void setSalePrice(String sp) {
+        this.salePrice = sp;
+    }
 
 }
